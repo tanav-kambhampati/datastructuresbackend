3b33c6804e15e776b7d74b0ca77126d695056821
AidanLau10 <aidanlau10@gmail.com>
2024-02-21 22:37:37 -0800

    create within init file

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from model.jobs import Job
+from model.jobusers import JobUsers
 import os
 
 """
@@ -27,3 +29,6 @@ Migrate(app, db)
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # maximum size of uploaded content
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']  # supported file types
 app.config['UPLOAD_FOLDER'] = 'volumes/uploads/'  # location of user uploaded content
+
+with app.app_context():
+    db.create_all()
