f70cbf84691cb74f705c857db25ecb7749ee57af
AidanLau10 <aidanlau10@gmail.com>
2024-02-22 20:37:18 -0800

    changes to init and main

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
+from flask_cors import CORS
 
 """
 These object can be used throughout project.
@@ -12,10 +13,11 @@ These object can be used throughout project.
 
 # Setup of key Flask object (app)
 app = Flask(__name__)
-cors = CORS(app, supports_credentials=True)
+
+cors = CORS(app, supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"])
 
 # Setup SQLAlchemy object and properties for the database (db)
-dbURI = 'sqlite:////volumes/sqlite.db'
+dbURI = 'sqlite:///volumes/sqlite.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
@@ -27,4 +29,3 @@ Migrate(app, db)
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # maximum size of uploaded content
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']  # supported file types
 app.config['UPLOAD_FOLDER'] = 'volumes/uploads/'  # location of user uploaded content
-
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import threading
-
-# import "packages" from flask
 from flask import render_template,request  # import render_template from "public" flask libraries
 from flask.cli import AppGroup
 
@@ -23,46 +20,67 @@ from api.jobuser import jobuser_api
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
 
-with app.app_context():
-    initUsers()
-    initPlayers()
-    initJobs()
-    initJobsUsers()
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
 
 # register URIs
-
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(job_api)
 app.register_blueprint(jobuser_api)
 app.register_blueprint(app_projects) # register app pages
 
+with app.app_context():
+    initUsers()
+    initPlayers()
+    initJobs()
+    initJobsUsers()
+
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
+
 @app.route('/')  # connects default URL to index() function
 def index():
     return render_template("index.html")
+
 
 @app.route('/table/')  # connects /stub/ URL to stub() function
 def table():
     return render_template("table.html")
 
+'''
 @app.before_request
 def before_request():
-    # Check if the request came from a specific origin
     allowed_origin = request.headers.get('Origin')
-    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io', 'http://127.0.0.1:4100/cptprojectfrontend/login/', 'http://127.0.0.1:4100/cptprojectfrontend/']:
+    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io', 'https://real-estate-analyzation.github.io']:
         cors._origins = allowed_origin
+    
+
+@app.after_request
+def after_request(response):
+    #response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
+    #allowed_origins = ['http://localhost:8080', 'http://localhost:4200', 'http://127.0.0.1:4200', 'https://nighthawkcoders.github.io', 'https://real-estate-analyzation.github.io']
+
+    #origin = request.headers.get('Origin')
+    #if origin and origin in allowed_origins:
+    #    response.headers.add('Access-Control-Allow-Origin', origin)
+
+    response.headers.add('Access-Control-Allow-Origin', request.headers.get('Origin'))
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
+'''
 
 # Create an AppGroup for custom commands
 custom_cli = AppGroup('custom', help='Custom commands')
 
 # Define a command to generate data
+
+
 @custom_cli.command('generate_data')
 def generate_data():
     initUsers()
@@ -70,10 +88,10 @@ def generate_data():
     initJobs()
     initJobsUsers()
 
+
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
-        
+
 # this runs the application on the development server
 if __name__ == "__main__":
-    # change name for testing
     app.run(debug=True, host="0.0.0.0", port="8064")
