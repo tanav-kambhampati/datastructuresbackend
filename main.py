ca43ec46b90a3961b42d3f0a62a77276b07b882f
AidanLau10 <aidanlau10@gmail.com>
2024-02-22 21:48:19 -0800

    cors changes

--- main.py
+++ main.py
@@ -60,20 +60,6 @@ def before_request():
         cors._origins = allowed_origin
     
 
-@app.after_request
-def after_request(response):
-    #response.headers.add('Access-Control-Allow-Origin', 'http://localhost:8080')
-    #allowed_origins = ['http://localhost:8080', 'http://localhost:4200', 'http://127.0.0.1:4200', 'https://nighthawkcoders.github.io', 'https://real-estate-analyzation.github.io']
-
-    #origin = request.headers.get('Origin')
-    #if origin and origin in allowed_origins:
-    #    response.headers.add('Access-Control-Allow-Origin', origin)
-
-    response.headers.add('Access-Control-Allow-Origin', request.headers.get('Origin'))
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    return response
 
 
 # Create an AppGroup for custom commands
