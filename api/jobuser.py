8a8f66a8e35872aedddd36e98ccac45010efadc8
AidanLau10 <aidanlau10@gmail.com>
2024-02-24 16:30:48 -0800

    revert to normal

--- api/jobuser.py
+++ api/jobuser.py
@@ -19,16 +19,7 @@ jobuser_api = Blueprint('jobuser_api', __name__,
 api = Api(jobuser_api)
 
 
-@app.after_request
-def after_request(response):
 
-    request_origin = request.headers.get('Origin')
-    if request_origin in ['http://127.0.0.1:4100/joblyFrontend/', 'http://localhost:4100/joblyFrontend/', 'https://aidanlau10.github.io/joblyFrontend/', 
-                          'https://aidanlau10.github.io/', 'http://127.0.0.1:4100/joblyFrontend/jobs/', 'http://localhost:4100/joblyFrontend/jobs/',
-                          'https://aidanlau10.github.io/joblyFrontend/jobs/', 'http://127.0.0.1:4100']:
-        response.headers['Access-Control-Allow-Origin'] = request_origin
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-    return response
 
 
 class JobUserAPI:        
